9ee3d622fe8f22d632be817e98454e6bfb429e46
jasmineflanders <jasminejoyflanders@gmail.com>
2022-04-07 00:33:29 -0400

    pushed the slay button

--- src/main/java/com/example/proj4/MainController.java
+++ src/main/java/com/example/proj4/MainController.java
@@ -1,16 +1,31 @@
 package com.example.proj4;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onDonutButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onDonutButtonClick(ActionEvent event) throws IOException {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("donut-view.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
