c7a1c4c5823da2ead87593501d1f646f48ab5d77
jasmineflanders <jasminejoyflanders@gmail.com>
2022-04-09 01:19:38 -0400

    working!!!

--- src/main/java/com/example/proj4/MainController.java
+++ src/main/java/com/example/proj4/MainController.java
@@ -20,39 +20,42 @@ public class MainController {
     private FXMLLoader coffeeLoader;
     private FXMLLoader orderViewLoader;
     private FXMLLoader storeOrderLoader;
-    private Parent donutViewRoot;
-    private Parent coffeeViewRoot;
-    private Parent orderViewRoot;
-    private Parent storeViewRoot;
+    private Scene donutViewScene;
+    private Scene coffeeViewScene;
     private Scene orderViewScene;
+    private Scene storeOrderViewScene;
 
     @FXML
     private void initialize() throws IOException {
+        donutLoader = new FXMLLoader(getClass().getResource("donut-view.fxml"));
+        donutLoader.load();
+        donutController = donutLoader.getController();
+        donutViewScene = new Scene(donutLoader.getRoot());
+
+        coffeeLoader = new FXMLLoader(getClass().getResource("coffee-view.fxml"));
+        coffeeLoader.load();
+        coffeeController = coffeeLoader.getController();
+        coffeeViewScene = new Scene(coffeeLoader.getRoot());
+
         orderViewLoader = new FXMLLoader(getClass().getResource("order-view.fxml"));
         orderViewLoader.load();
         orderViewController = orderViewLoader.getController();
         orderViewScene = new Scene(orderViewLoader.getRoot());
-        //orderViewController.setMainController(this);
-        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("store-order-view.fxml"));
-        loader2.load();
-        StoreOrderViewController storeOrderViewController = loader2.getController();
-        storeOrderViewController.setMainController(this);
-        donutLoader = new FXMLLoader(getClass().getResource("donut-view.fxml"));
-        donutLoader.load();
-        donutController = donutLoader.getController();
+
+        storeOrderLoader = new FXMLLoader(getClass().getResource("store-order-view.fxml"));
+        storeOrderLoader.load();
+        storeOrderViewController = storeOrderLoader.getController();
+        storeOrderViewScene = new Scene(storeOrderLoader.getRoot());
+
         donutController.setOrderViewController(this.orderViewController);
-        FXMLLoader loader4 = new FXMLLoader(getClass().getResource("coffee-view.fxml"));
-        loader4.load();
-        CoffeeController coffeeController = loader4.getController();
-        coffeeController.setMainController(this);
+
     }
 
     @FXML
     protected void onDonutButtonClick(ActionEvent event) throws IOException {
-
         try {
             Stage stage = new Stage();
-            stage.setScene(new Scene(donutLoader.getRoot()));
+            stage.setScene(donutViewScene);
             stage.show();
         } catch(Exception e) {
             e.printStackTrace();
@@ -62,10 +65,8 @@ public class MainController {
     @FXML
     protected void onCoffeeButtonClick(ActionEvent event) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("coffee-view.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage.setScene(new Scene(root1));
+            stage.setScene(coffeeViewScene);
             stage.show();
         } catch(Exception e) {
             e.printStackTrace();
@@ -87,10 +88,9 @@ public class MainController {
     @FXML
     protected void onStoreOrderButtonClick(ActionEvent event) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("store-order-view.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage.setScene(new Scene(root1));
+            stage.setScene(storeOrderViewScene);
+            //storeOrderViewController.updateListView();
             stage.show();
         } catch(Exception e) {
             e.printStackTrace();
